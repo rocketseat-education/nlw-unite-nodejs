cd95abd8d201aa4747b4d5e769d8a5271adc75fe
Elias Gabriel <eliasgabrielcf@gmail.com>
2024-04-04 14:05:52 -0300

    feat: change to use postgres

--- prisma/migrations/20240326182647_create_table_attendees/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "attendees" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "event_id" TEXT NOT NULL,
-    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
