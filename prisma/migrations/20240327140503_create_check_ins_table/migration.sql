f0f65807c765b57f4f1a9805181854a17b2c976d
Rafael Vinícius Schneider <rafaelvschneider@gmail.com>
2024-04-06 10:32:55 -0300

    feat(database): adapt from SQLite to PostgreSQL syntax

--- prisma/migrations/20240327140503_create_check_ins_table/migration.sql
+++ prisma/migrations/20240327140503_create_check_ins_table/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "check_ins" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "attendee_id" INTEGER NOT NULL,
     CONSTRAINT "check_ins_attendee_id_fkey" FOREIGN KEY ("attendee_id") REFERENCES "attendees" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
--- prisma/migrations/20240327143815_add_cascades/migration.sql
+++ prisma/migrations/20240327143815_add_cascades/migration.sql
@@ -1,8 +1,8 @@
 -- RedefineTables
 PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_check_ins" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" SERIAL NOT NULL PRIMARY KEY,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "attendee_id" INTEGER NOT NULL,
     CONSTRAINT "check_ins_attendee_id_fkey" FOREIGN KEY ("attendee_id") REFERENCES "attendees" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
@@ -11,10 +11,10 @@ DROP TABLE "check_ins";
 ALTER TABLE "new_check_ins" RENAME TO "check_ins";
 CREATE UNIQUE INDEX "check_ins_attendee_id_key" ON "check_ins"("attendee_id");
 CREATE TABLE "new_attendees" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" SERIAL NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "event_id" TEXT NOT NULL,
     CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
