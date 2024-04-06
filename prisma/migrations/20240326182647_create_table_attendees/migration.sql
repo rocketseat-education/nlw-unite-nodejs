40db07e77c779b7b864083a5548aa57e394fa22c
Rafael Vinícius Schneider <rafaelvschneider@gmail.com>
2024-04-06 17:08:43 -0300

    feat(database): recreate migrations from SQLite to PostgreSQL

--- prisma/migrations/20240326182647_create_table_attendees/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "attendees" (
-    "id" SERIAL NOT NULL PRIMARY KEY,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "event_id" TEXT NOT NULL,
-    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
-);
--- prisma/migrations/20240326184908_add_uniqueness_on_event_id_and_email/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[event_id,email]` on the table `attendees` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees"("event_id", "email");
